833abd24ee621a428eb0d074fd32ec5b12ff0024
Mehdzii <mehdi.chanoufi1@gmail.com>
2026-08-20 16:12:36 +0100

    Support 50mb payload limit for high quality product picture uploads

--- index.js
+++ index.js
@@ -7,7 +7,8 @@ dotenv.config();
 
 const app = express();
 app.use(cors());
-app.use(express.json({ limit: '10mb' }));
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ limit: '50mb', extended: true }));
 
 const PORT = process.env.PORT || 5000;
 const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost:27017/hanut_db';
